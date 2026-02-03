a90fd4b41a8dba9b7c8be13fdacb589ad984e2d7
Trent <beachbear6@gmail.com>
2026-02-03 13:55:35 -0600

    added calendars.py and finished timedeltas.py

--- 04_timedeltas.py
+++ 04_timedeltas.py
@@ -4,7 +4,7 @@ from datetime import date
 from datetime import datetime
 from datetime import timedelta
 
-# construct a basic timiedelta and print it
+# construct a basic timedelta and print it
 
 print(timedelta(days=365,hours=5,minutes=1))
 
@@ -15,3 +15,23 @@ print(f"Today is {now}")
 
 # print today's date one year from now
 print(f"In one year it will be: {now+timedelta(days=365)}")
+
+# create a timedelta that uses more than one argument
+print(f"In two weeks and 3 days it will be: {now + timedelta(weeks=3,days=3)}")
+
+#calculate the date 1 week agon, formatted as a string
+print(f"One week ago it was: {now-timedelta(weeks=1)}")
+
+t = datetime.now() - timedelta(weeks=2)
+s = t.strftime("%A %B %d, %Y")
+print(f"Two weeks ago it was: {s}")
+
+# How many days until April Fools' Day?
+today = date.today()
+afd = date(today.year,month=4,day=1)
+if afd < today:
+    print(f"April fools' day already went by {(today-afd).days} days ago")
+    afd = afd.replace(year=today.year+1)
+
+time_to_afd = afd-today
+print(f"It's just {time_to_afd.days} days until April Fools' Day")
